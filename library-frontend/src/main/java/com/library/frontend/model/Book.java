52ba69941a1a6d8fbdf36a0a1ac5652536e1753c
Macwinner <ifechukwuw@gmail.com>
2026-01-15 08:39:04 +0000

    Modified the Book model to separate JavaFX properties from the actual data fields using @Expose annotations

--- library-frontend/src/main/java/com/library/frontend/model/Book.java
+++ library-frontend/src/main/java/com/library/frontend/model/Book.java
@@ -1,92 +1,124 @@
 package com.library.frontend.model;
 
+import com.google.gson.annotations.Expose;
 import javafx.beans.property.*;
 
 import java.time.LocalDate;
 
 public class Book {
 
-    private final LongProperty id;
-    private final StringProperty title;
-    private final StringProperty author;
-    private final StringProperty isbn;
-    private final ObjectProperty<LocalDate> publishedDate;
+    @Expose
+    private Long id;
+    @Expose
+    private String title;
+    @Expose
+    private String author;
+    @Expose
+    private String isbn;
+    @Expose
+    private LocalDate publishedDate;
+
+    private transient LongProperty idProperty;
+    private transient StringProperty titleProperty;
+    private transient StringProperty authorProperty;
+    private transient StringProperty isbnProperty;
+    private transient ObjectProperty<LocalDate> publishedDateProperty;
 
     public Book() {
         this(null, "", "", "", null);
     }
 
     public Book(Long id, String title, String author, String isbn, LocalDate publishedDate) {
-        this.id = new SimpleLongProperty(id != null ? id : 0);
-        this.title = new SimpleStringProperty(title);
-        this.author = new SimpleStringProperty(author);
-        this.isbn = new SimpleStringProperty(isbn);
-        this.publishedDate = new SimpleObjectProperty<>(publishedDate);
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publishedDate = publishedDate;
     }
 
     // ID Property
     public Long getId() {
-        return id.get();
+        return id;
     }
 
     public void setId(Long value) {
-        id.set(value);
+        this.id = value;
+        idProperty().set(value != null ? value : 0);
     }
 
     public LongProperty idProperty() {
-        return id;
+        if (idProperty == null) {
+            idProperty = new SimpleLongProperty(this, "id", id != null ? id : 0);
+        }
+        return idProperty;
     }
 
     // Title Property
     public String getTitle() {
-        return title.get();
+        return title;
     }
 
     public void setTitle(String value) {
-        title.set(value);
+        this.title = value;
+        titleProperty().set(value);
     }
 
     public StringProperty titleProperty() {
-        return title;
+        if (titleProperty == null) {
+            titleProperty = new SimpleStringProperty(this, "title", title);
+        }
+        return titleProperty;
     }
 
     // Author Property
     public String getAuthor() {
-        return author.get();
+        return author;
     }
 
     public void setAuthor(String value) {
-        author.set(value);
+        this.author = value;
+        authorProperty().set(value);
     }
 
     public StringProperty authorProperty() {
-        return author;
+        if (authorProperty == null) {
+            authorProperty = new SimpleStringProperty(this, "author", author);
+        }
+        return authorProperty;
     }
 
     // ISBN Property
     public String getIsbn() {
-        return isbn.get();
+        return isbn;
     }
 
     public void setIsbn(String value) {
-        isbn.set(value);
+        this.isbn = value;
+        isbnProperty().set(value);
     }
 
     public StringProperty isbnProperty() {
-        return isbn;
+        if (isbnProperty == null) {
+            isbnProperty = new SimpleStringProperty(this, "isbn", isbn);
+        }
+        return isbnProperty;
     }
 
     // Published Date Property
     public LocalDate getPublishedDate() {
-        return publishedDate.get();
+        return publishedDate;
     }
 
     public void setPublishedDate(LocalDate value) {
-        publishedDate.set(value);
+        this.publishedDate = value;
+        publishedDateProperty().set(value);
     }
 
     public ObjectProperty<LocalDate> publishedDateProperty() {
-        return publishedDate;
+        if (publishedDateProperty == null) {
+            publishedDateProperty = new SimpleObjectProperty<>(this, "publishedDate", publishedDate);
+        }
+        return publishedDateProperty;
     }
 
     @Override
